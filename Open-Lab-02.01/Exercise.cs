a0658afe8bb49298fab608b08f177f38516e89b9
samuelmalik <113421307+samuelmalik@users.noreply.github.com>
2022-09-17 18:01:49 +0200

    Update Exercise.cs

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._01
 {
@@ -6,11 +6,14 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
+            double[] numbers = { 45.8, 54.7, 420.69, 28.4, 112.9875};
+            return numbers;
             throw new NotImplementedException();
         }
 
         public double GetSecond(double[] numbers)
         {
+            return numbers[1];
             throw new NotImplementedException();
         }
     }
